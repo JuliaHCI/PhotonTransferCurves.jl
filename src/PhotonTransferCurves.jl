9682a70c0ded1ec246ef79f9e3a81dab843028ef
Miles Lucas <mdlucas@hawaii.edu>
2022-09-13 22:50:52 -1000

    reorganize files

--- src/PhotonTransferCurves.jl
+++ src/PhotonTransferCurves.jl
@@ -1,31 +1,7 @@
 module PhotonTransferCurves
 
-using Distributions
-using Random
-using Statistics
-using StatsBase
-
-export testdata, ShotNoise, ReadNoise, noise
-
-img_to_curve(cube::AbstractArray; dims) = map(img_to_curve, eachslice(cube; dims))
-img_to_curve(img::AbstractMatrix) = mean_and_std(img)
-img_to_curve(pair::Tuple) = pair
-
-function signal_and_noise(cube::AbstractArray; dims)
-    data = img_to_curve(cube; dims)
-    S = map(first, data)
-    σ = map(last, data)
-    return S, σ
-end
-
-function signal_and_noise(images::Vararg{<:AbstractMatrix})
-    data = map(img_to_curve, images)
-    S = map(first, data)
-    σ = map(last, data)
-    return S, σ
-end
-
-include("data.jl")
-include("sources.jl")
+include("statistics.jl")
+include("models.jl")
+include("fitting.jl")
 
 end
---
+++ src/statistics.jl
@@ -0,0 +1,34 @@
+# Functions for measuring the signal and noise of typical
+# CMOS and CCD data
+using BiweightStats
+
+"""
+"""
+function process_frames(frames::Vararg{<:AbstractMatric}; kwargs...)
+    map(f -> process_single_frame(f; kwargs...), frames)
+end
+function process_frames(cube::AbstractArray{T,3}; dims=3, kwargs...) where T
+    map(f -> process_single_frame(f; kwargs...), eachslice(cube; dims=dims))
+end
+
+function process_single_frame(frame::AbstractMatrix; window_size=30, kwargs...)
+    window_size = _normalize_window_size(window_size)
+    output =
+end
+
+_normalize_window_size(n::NTuple{2, <:Integer}) = n
+_normalize_window_size(n::Integer) = (n, n)
+
+"""
+    signal_and_noise(data; kwargs...)
+
+Measure the signal and variance of `data` using [`BiweightStats.location`](@ref) and [`BiweightStats.midvar`](@ref). These statistics are robust to outliers, and their level of filtering can be set with keyword arguments, see the function reference for more information.
+
+# See also
+[`BiweightStats.location`](@ref), [`BiweightStats.midvar`](@ref)
+"""
+function signal_and_noise(data; kwargs...)
+    signal = location(data; kwargs...)
+    var = midvar(data; M=signal, kwargs...)
+    return signal, var
+end
